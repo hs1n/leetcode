bba3f06f76e8a75b15538a7d75a2d47bde769030
Hsin <bar1eycool@gmail.com>
2022-04-06 20:16:54 +0800

    Daily Update (0026)

---
+++ src/main/java/leetcode/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,25 @@
+package leetcode.easy;
+
+import java.util.Arrays;
+
+/**
+ * @see <a href='https://leetcode.com/problems/remove-duplicates-from-sorted-array/'>26. Remove Duplicates from Sorted Array</a>
+ */
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 1) {
+            return 1;
+        } else if (nums.length > 1 && nums.length <= 30000) {
+            int r = 0;
+            for (int i = 1; i < nums.length; i++) {
+                if (nums[r] != nums[i]) {
+                    r++;
+                    nums[r] = nums[i];
+                }
+            }
+            return r + 1;
+        } else {
+            throw new IllegalArgumentException("int[] length error");
+        }
+    }
+}
---
+++ src/test/java/leetcode/easy/ut/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,29 @@
+package leetcode.easy.ut;
+
+import leetcode.easy.RemoveDuplicatesFromSortedArray;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveDuplicatesFromSortedArrayTest {
+
+    private final RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new RemoveDuplicatesFromSortedArray();
+
+    @Test
+    void example01() {
+        examination(new int[]{1, 1, 2}, new int[]{1, 2});
+    }
+
+    @Test
+    void example02() {
+        examination(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}, new int[]{0, 1, 2, 3, 4});
+    }
+
+    private void examination(int[] nums, int[] expectedNums) {
+        int k = removeDuplicatesFromSortedArray.removeDuplicates(nums);
+        assertEquals(k, expectedNums.length);
+        for (int i = 0; i < k; i++) {
+            assertEquals(nums[i], expectedNums[i]);
+        }
+    }
+}
