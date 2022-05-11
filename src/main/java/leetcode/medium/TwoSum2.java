925b72e3a3773a202946cd2b1ada57f5cd0fc622
Hsin <bar1eycool@gmail.com>
2022-05-11 21:24:15 +0800

    Daily Update (0167)

---
+++ src/main/java/leetcode/medium/TwoSum2.java
@@ -0,0 +1,24 @@
+package leetcode.medium;
+
+
+
+/**
+ * @see <a href="https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/">167. Two Sum II - Input Array Is Sorted</a>
+ */
+
+public class TwoSum2 {
+    public int[] twoSum(int[] numbers, int target) {
+        int start = 0, end = numbers.length - 1;
+
+        while (start < end) {
+            if (numbers[start] + numbers[end] == target) {
+                return new int[]{start + 1, end + 1};
+            } else if (numbers[start] + numbers[end] < target) {
+                start++;
+            } else {
+                end--;
+            }
+        }
+        return new int[]{start + 1, end + 1};
+    }
+}
---
+++ src/test/java/leetcode/medium/TwoSum2Test.java
@@ -0,0 +1,25 @@
+package leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSum2Test {
+    TwoSum2 twoSum2 = new TwoSum2();
+
+    @Test
+    void example01() {
+        assertArrayEquals(twoSum2.twoSum(new int[]{2, 7, 11, 15}, 9), new int[]{1, 2});
+    }
+
+    @Test
+    void example02() {
+        assertArrayEquals(twoSum2.twoSum(new int[]{2, 3, 4}, 6), new int[]{1, 3});
+    }
+
+    @Test
+    void example03() {
+        assertArrayEquals(twoSum2.twoSum(new int[]{-1, 0}, -1), new int[]{1, 2});
+    }
+
+}
