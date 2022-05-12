43f3a7118fa3bbd18bf0a44d4b989c2a63101dee
Hsin <bar1eycool@gmail.com>
2022-05-12 22:08:12 +0800

    inner class adjustment

--- src/main/java/leetcode/easy/MergeTwoSortedLists.java
+++ src/main/java/leetcode/easy/MergeTwoSortedLists.java
@@ -20,21 +20,21 @@ class MergeTwoSortedLists {
             return list2;
         }
     }
-}
 
-class ListNode {
-    int val;
-    ListNode next;
+    public static class ListNode {
+        int val;
+        ListNode next;
 
-    ListNode() {
-    }
+        ListNode() {
+        }
 
-    ListNode(int val) {
-        this.val = val;
-    }
+        ListNode(int val) {
+            this.val = val;
+        }
 
-    ListNode(int val, ListNode next) {
-        this.val = val;
-        this.next = next;
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
